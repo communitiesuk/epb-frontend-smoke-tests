47e9bd0d56d9939105d8512815549a2abbbb4940
Christine Horrocks <splogy2001@hotmail.com>
2022-01-06 16:38:25 +0000

    Reduce domestic tests and added non domestic tests

--- spec/find_non_domestic_certificate_by_postcode_spec.rb
+++ spec/find_non_domestic_certificate_by_postcode_spec.rb
@@ -1,37 +1,37 @@
 # frozen_string_literal: true
 
-describe 'Find Domestic Certificate By Postcode in English', type: :feature, journey: true do
+describe 'Find Non-Domestic Certificate By Postcode in English', type: :feature, journey: true do
   context 'when searching for a domestic certificate' do
     before do
       visit 'https://www.gov.uk/find-energy-certificate'
       click_on 'Start now'
-      find('#label-domestic').click
+      find('#label-non-domestic').click
       click_on 'Continue'
-      fill_in 'postcode', with: 'HP17 0UZ'
+      fill_in 'postcode', with: 'SW1A 2AA'
       click_button 'Find'
-      click_on '3 Gardeners Cottages,'
+      click_on 'DEC', match: :first
     end
 
     it 'shows the certificate with the expected header' do
-      expect(page).to have_content 'Energy performance certificate (EPC)'
+      expect(page).to have_content 'Display energy certificate (DEC)'
     end
   end
 end
 
-describe 'Find Domestic Certificate By Postcode in Welsh', type: :feature, journey: true do
+describe 'Find Non-Domestic Certificate By Postcode in Welsh', type: :feature, journey: true do
   # TODO: When we have a Welsh homepage update this section to start there
   context 'when searching for a domestic certificate in Welsh' do
     before do
       visit 'https://find-energy-certificate.service.gov.uk/find-a-certificate/type-of-property?lang=cy'
-      find('#label-domestic').click
+      find('#label-non-domestic').click
       click_on 'Parhau'
-      fill_in 'postcode', with: 'HP17 0UZ'
+      fill_in 'postcode', with: 'SW1A 2AA'
       click_button 'Chwiliwch'
-      click_on '3 Gardeners Cottages,'
+      click_on 'DEC', match: :first
     end
 
     it 'shows the certificate with the expected header' do
-      expect(page).to have_content 'Tystysgrif perfformiad ynni (EPC)'
+      expect(page).to have_content 'Tystysgrif ynni i’w harddangos (DEC)'
     end
   end
 end
