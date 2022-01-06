5fad04aa1c042aa6b6268408ea20e4cbc3c63e0d
Christine Horrocks <splogy2001@hotmail.com>
2022-01-06 17:35:27 +0000

    Added find by street and town

---
+++ spec/find_domestic_certificate_by_street_and_town_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+describe 'Find Domestic Certificate By street and town in English', type: :feature, journey: true do
+  context 'when searching for a domestic certificate' do
+    before do
+      visit 'https://www.gov.uk/find-energy-certificate'
+      click_on 'Start now'
+      find('#label-domestic').click
+      click_on 'Continue'
+      click_on 'find an EPC using the street name and town'
+      fill_in 'street_name', with: 'Bamburgh Castle'
+      fill_in 'town', with: 'Bamburgh'
+      click_button 'Find'
+      click_on 'The Keep'
+    end
+
+    it 'shows the certificate with the expected header' do
+      expect(page).to have_content 'Energy performance certificate (EPC)'
+    end
+  end
+end
+
+describe 'Find Domestic Certificate By street and town in Welsh', type: :feature, journey: true do
+  # TODO: When we have a Welsh homepage update this section to start there
+  context 'when searching for a domestic certificate in Welsh' do
+    before do
+      visit 'https://find-energy-certificate.service.gov.uk/find-a-certificate/type-of-property?lang=cy'
+      find('#label-domestic').click
+      click_on 'Parhau'
+      click_on 'chwiliwch am EPC drwy ddefnyddio enw’r stryd a’r dref'
+      fill_in 'street_name', with: 'Bamburgh Castle'
+      fill_in 'town', with: 'Bamburgh'
+      click_button 'Chwiliwch'
+      click_on 'The Keep'
+    end
+
+    it 'shows the certificate with the expected header' do
+      expect(page).to have_content 'Tystysgrif perfformiad ynni (EPC)'
+    end
+  end
+end
---
+++ spec/find_non_domestic_certificate_by_street_and_town_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+describe 'Find Non-Domestic Certificate By street and town in English', type: :feature, journey: true do
+  context 'when searching for a domestic certificate' do
+    before do
+      visit 'https://www.gov.uk/find-energy-certificate'
+      click_on 'Start now'
+      find('#label-non-domestic').click
+      click_on 'Continue'
+      click_on 'find energy certificates and reports using the street name and town'
+      fill_in 'street_name', with: 'Prime Minister & First Lord of the Treasury'
+      fill_in 'town', with: 'London'
+      click_button 'Find'
+      click_on 'DEC', match: :first
+    end
+
+    it 'shows the certificate with the expected header' do
+      expect(page).to have_content 'Display energy certificate (DEC)'
+    end
+  end
+end
+
+# describe 'Find Non-Domestic Certificate By street and town in Welsh', type: :feature, journey: true do
+#   # TODO: When we have a Welsh homepage update this section to start there
+#   context 'when searching for a domestic certificate in Welsh' do
+#     before do
+#       visit 'https://find-energy-certificate.service.gov.uk/find-a-certificate/type-of-property?lang=cy'
+#       find('#label-domestic').click
+#       click_on 'Parhau'
+#       click_on 'chwiliwch am dystysgrifau perfformiad ynni drwy ddefnyddio enw’r stryd a’r dref'
+#       fill_in 'street_name', with: 'Prime Minister & First Lord of the Treasury'
+#       fill_in 'town', with: 'London'
+#       click_button 'Chwiliwch'
+#       click_on 'DEC', match: :first
+#     end
+#
+#     it 'shows the certificate with the expected header' do
+#       expect(page).to have_content 'Tystysgrif ynni i’w harddangos (DEC)'
+#     end
+#   end
+# end
