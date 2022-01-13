bb7fca7522f09b6681abf04ecc62ec0e37e8a3af
Barry Halper <barryhalper@gmail.com>
2022-01-13 16:34:51 +0000

    EPBR-1334 Changed file name of worker smoke test class

---
+++ sidekiq/worker/production_frontend_smoke_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'sidekiq'
+
+module Worker
+  class ProductionFrontEndSmokeTest
+    include Sidekiq::Worker
+
+    def perform
+      pp "I'm running!"
+      # system('bundle exec rspec spec')
+    end
+  end
+end
