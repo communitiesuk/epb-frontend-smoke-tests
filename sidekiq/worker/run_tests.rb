440885784144fa12ae40ff3dbfa5f4e9574016e4
Barry Halper <barryhalper@gmail.com>
2022-01-24 16:42:32 +0000

    EPBR-1334 updated worker to not use rake

--- sidekiq/worker/run_tests.rb
+++ sidekiq/worker/run_tests.rb
@@ -9,7 +9,7 @@ module Worker
     sidekiq_options retry: false
 
     def perform
-      rake_task('spec').invoke
+      system('rspec spec/journey/*_spec.rb --format json --out logs/output.json --format documentation')
       rspec_output = JSON.load_file('logs/output.json')
       status_check = JourneyTestStatusCheck.new(rspec_output: rspec_output, slack_gateway: SlackGateway.new)
       status_check.format_and_send_errors if status_check.failure_count >= 1
