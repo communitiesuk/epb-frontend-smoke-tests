e63e690366a8f9ee33d7a5d06d50c7e254fdfdb5
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2023-10-04 21:02:33 +0100

    Update integration URLs now PaaS domains are gone

--- cypress.config.js
+++ cypress.config.js
@@ -4,20 +4,20 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   env: {
     get_service_en_integration:
-      'https://mhclg-epb-static-start-pages-integration.london.cloudapps.digital/getting-a-new-energy-certificate.html',
-    get_domain_integration: 'https://getting-new-energy-certificate-integration.london.cloudapps.digital/',
+      'http://epb-static-start-pages-integration.s3-website.eu-west-2.amazonaws.com/getting-a-new-energy-certificate.html',
+    get_domain_integration: 'https://getting-new-energy-certificate-integration.digital.communities.gov.uk/',
     get_service_en_production: 'https://www.gov.uk/get-new-energy-certificate',
     get_domain_production: 'https://getting-new-energy-certificate.service.gov.uk/',
     find_service_en_integration:
-      'https://mhclg-epb-static-start-pages-integration.london.cloudapps.digital/find-an-energy-certificate.html',
-    find_domain_integration: 'https://find-energy-certificate-integration.london.cloudapps.digital/',
+      'http://epb-static-start-pages-integration.s3-website.eu-west-2.amazonaws.com/find-an-energy-certificate.html',
+    find_domain_integration: 'https://find-energy-certificate-integration.digital.communities.gov.uk/',
     find_service_en_production: 'https://www.gov.uk/find-energy-certificate',
     find_domain_production: 'https://find-energy-certificate.service.gov.uk/',
     get_service_cy_integration:
-      'https://mhclg-epb-static-start-pages-integration.london.cloudapps.digital/sicrhau-tystysgrif-ynni-newydd.html',
+      'http://epb-static-start-pages-integration.s3-website.eu-west-2.amazonaws.com/sicrhau-tystysgrif-ynni-newydd.html',
     get_service_cy_production: 'https://www.gov.uk/cael-tystysgrif-ynni-newydd',
     find_service_cy_integration:
-      'https://mhclg-epb-static-start-pages-integration.london.cloudapps.digital/chwiliwch-am-dystysgrif-ynni.html',
+      'http://epb-static-start-pages-integration.s3-website.eu-west-2.amazonaws.com/chwiliwch-am-dystysgrif-ynni.html',
     find_service_cy_production:
       'https://www.gov.uk/dod-o-hyd-i-dystysgrif-ynni',
   },
